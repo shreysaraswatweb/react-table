919aeb6498a7a832c4019185f2cb6cab7deb2454
shreysaraswatweb <77667746+shreysaraswatweb@users.noreply.github.com>
2025-10-03 03:18:27 +0530

    Login Page design

--- src/App.jsx
+++ src/App.jsx
@@ -43,6 +43,37 @@ function App() {
                 </a>
               </div>
             </form>
+             <form action="" className="sign-up-form">
+              <h2 className="title">Sign up</h2>
+              <div className="input-field">
+                <i className="fas fa-user"></i>
+                <input type="text" placeholder="Username" />
+              </div>
+               <div className="input-field">
+                <i className="fas fa-envelope"></i>
+                <input type="text" placeholder="Email" />
+              </div>
+              <div className="input-field">
+                <i className="fas fa-lock"></i>
+                <input type="password" placeholder="Password" />
+              </div>
+              <input type="submit" value="Sign up" className="btn solid" />
+              <p className="social-text">Or Sign up with social platforms</p>
+              <div className="social-media">
+                <a href="#" className="social-icon">
+                  <i className="fab fa-facebook-f"></i>
+                </a>
+                <a href="#" className="social-icon">
+                  <i className="fab fa-twitter"></i>
+                </a>
+                <a href="#" className="social-icon">
+                  <i className="fab fa-google"></i>
+                </a>
+                <a href="#" className="social-icon">
+                  <i className="fab fa-linkedin-in"></i>
+                </a>
+              </div>
+            </form>
           </div>
         </div>
         <div className="panels-container"></div>
