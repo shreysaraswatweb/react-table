f58e4becb2ee8e85acf787169563a7d817fc61cf
shreysaraswatweb <77667746+shreysaraswatweb@users.noreply.github.com>
2025-10-02 13:39:38 +0530

    Project added

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="bg-violet-500 text-4xl text-white">
+        CODE ON
+      </h1>
+      <div className="animate-pulse text-7xl">
+        😊
+      </div>
+    </>
+  );
+}
+
+export default App;
