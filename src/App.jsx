2bf45ec3ccb81f7a63e203017a87e637a321a15c
shreysaraswatweb <77667746+shreysaraswatweb@users.noreply.github.com>
2025-10-03 03:10:51 +0530

    Login Page design

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,46 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1 className="bg-violet-500 text-4xl text-white">
+      {/* <h1 className="bg-violet-500 text-4xl text-white">
         CODE ON
       </h1>
       <div className="animate-pulse text-7xl">
         😊
+      </div> */}
+
+      <div className="container">
+        <div className="forms-container">
+          <div className="signin-signup">
+            <form action="" className="sign-in-form">
+              <h2 className="title">Sign in</h2>
+              <div className="input-field">
+                <i className="fas fa-user"></i>
+                <input type="text" placeholder="Username" />
+              </div>
+              <div className="input-field">
+                <i className="fas fa-lock"></i>
+                <input type="password" placeholder="Password" />
+              </div>
+              <input type="submit" value="Login" className="btn solid" />
+              <p className="social-text">Or Sign in with social platforms</p>
+              <div className="social-media">
+                <a href="#" className="social-icon">
+                  <i className="fab fa-facebook-f"></i>
+                </a>
+                <a href="#" className="social-icon">
+                  <i className="fab fa-twitter"></i>
+                </a>
+                <a href="#" className="social-icon">
+                  <i className="fab fa-google"></i>
+                </a>
+                <a href="#" className="social-icon">
+                  <i className="fab fa-linkedin-in"></i>
+                </a>
+              </div>
+            </form>
+          </div>
+        </div>
+        <div className="panels-container"></div>
       </div>
     </>
   );
